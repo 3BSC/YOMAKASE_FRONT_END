45de3ce078c3905a1b2fca56a5f6ffacf598fa4b
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-03-06 14:50:33 +0900

    style: add style to Login component

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-
+import "./Login.Style.scss";
 const Login = () => {
   const [isCeoLogin, setCeoLogin] = useState(false);
   const userEmail = useRef();
@@ -23,31 +23,47 @@ const Login = () => {
     return;
   };
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
+    <div className="Container">
+      <form onSubmit={handleSubmit} className="LoginContents">
+        <div className="LoginForm">
           <label htmlFor="user-email">이메일</label>
           <br />
-          <input type="text" id="user-email" ref={userEmail}></input>
+          <input
+            type="text"
+            id="user-email"
+            className="LoginInput"
+            ref={userEmail}
+          ></input>
         </div>
-        <div>
+        <div className="LoginForm">
           <label htmlFor="user-password">비밀번호</label>
           <br />
-          <input type="password" ref={userPassword}></input>
+          <input
+            type="password"
+            ref={userPassword}
+            className="LoginInput"
+          ></input>
         </div>
-        <div>
+        <div className="LoginOption">
           <input
             type="checkbox"
             id="user-type"
             onClick={() => setCeoLogin(!isCeoLogin)}
           ></input>
           <label htmlFor="user-type">사장님 계정</label>
+          <span> /</span>
           <input type="checkbox" id="auto-login"></input>
           <label htmlFor="auto-login">자동 로그인</label>
         </div>
-        <button type="submit">로그인</button>
+        <div className="ButtonContainer">
+          <button type="submit" className="Loginbutton">
+            로그인
+          </button>
+          <button className="Loginbutton">회원가입</button>
+          <button className="Loginbutton">이메일/비밀번호 찾기</button>
+        </div>
       </form>
-    </>
+    </div>
   );
 };
 
