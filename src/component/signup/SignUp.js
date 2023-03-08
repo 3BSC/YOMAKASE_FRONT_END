b71d36d912b14d0e7fa6840fb61af756653eadc0
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-03-08 15:12:57 +0900

    feat: add button for signup submit

--- src/component/signup/SignUp.js
+++ src/component/signup/SignUp.js
@@ -5,18 +5,21 @@ import OwnerForm from "./OwnerForm";
 const SignUp = () => {
   const [isOwnerForm, setOwnerForm] = useState(false);
   return (
-    <form>
-      <NormalForm />
-      <div>
-        <input
-          type="checkbox"
-          id="checkOwner"
-          onClick={() => setOwnerForm(!isOwnerForm)}
-        />
-        <label htmlFor="checkOwner">사장님 계정으로 가입하시나요?</label>
-      </div>
-      {isOwnerForm && <OwnerForm />}
-    </form>
+    <>
+      <form>
+        <NormalForm />
+        <div>
+          <input
+            type="checkbox"
+            id="checkOwner"
+            onClick={() => setOwnerForm(!isOwnerForm)}
+          />
+          <label htmlFor="checkOwner">사장님 계정으로 가입하시나요?</label>
+        </div>
+        {isOwnerForm && <OwnerForm />}
+      </form>
+      <button>회원가입</button>
+    </>
   );
 };
 
