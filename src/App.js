81387dc5a7f554c33f60ba0b727e0bfb64234417
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2023-04-10 16:07:47 +0900

    chore: add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      테스트
-    </div>
+    <Routes>
+      <Route path="/" element={<div>메인</div>} />
+      <Route path="/login" element={<div>로그인</div>} />
+      <Route path="/signup" element={<div>회원가입</div>} />
+      <Route path="/find-password" element={<div>비밀번호 찾기</div>} />
+    </Routes>
   );
 }
 
